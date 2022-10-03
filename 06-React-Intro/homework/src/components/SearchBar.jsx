3a3a572a4c979ca5d23ffb8cf69aa629bf3cadeb
Sebastian <101646608+SeboV96@users.noreply.github.com>
2022-10-03 17:23:47 -0300

    Add files via upload

---
+++ 06-React-Intro/homework/src/components/SearchBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function SearchBar(props) {
+  // acá va tu código
+  return (
+  <div>
+    <input type='text' />
+    <button onClick={() => props.onSearch("Cairns")}>Agregar</button>
+  </div>
+  );
+};
